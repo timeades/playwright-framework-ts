691cf7447514a3d61fb0efe7330d8f1781040b25
timeades <tim.eades@mac.com>
2023-01-07 21:03:00 +0000

    additions to framewrok

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { baseUrl } from '../tests/utils/constants/urls'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(`${baseUrl}`);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(`${baseUrl}`);
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
