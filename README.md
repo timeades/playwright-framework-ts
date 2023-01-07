0c6054f6ce89f67a1c9ec8411e87ceabcb88a663
timeades <tim.eades@mac.com>
2023-01-07 14:55:03 +0000

    set framework, reporters, browsers and skeleton for tests

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # playwright-framework-ts
 Skeleton set-up of framework, reporters, run environment, target browsers
+
+# Running the tests
+To run the tests use the command: 
+
+'npx playwright test'
+
+As the tests cases grow, running locally would be split into seperate functional area to make debugging and re-running specific areas individulay easier and less time consuming.
+
+The current configuration of the run file runs the tests against desktop browsers: Chrome, Firefox & Safari. They are also run against mobile web browsers Chrome & Safari on a Pixel 5 & iPhone 12
+
+# Viewing the test results
+Once the tests are run you can view the results running the command: 
+
+'npx playwright show-report'
+
+The current configuration for the test reporter is the default Playwright one. This could be updated depending on who needs access to results and the information required. Alternatives include Allure, Monocart etc.
+
+Current test results view:
+![Image of the test report page](/images/testResults.png)
+
+
+
+
---
+++ tests/loginPage-functional.tests.ts
@@ -0,0 +1,52 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+// Verify if a user will be able to login with a valid username and valid password.
+test('can login', async ({ page }) => {
+
+});
+
+// Verify if a user cannot login with a valid username and an invalid password.
+test('', async ({ page }) => {
+
+});
+
+// Verify the login page for both, when the field is blank and Submit button is clicked.
+test('', async ({ page }) => {
+
+});
+
+// Verify the ‘Forgot Password’ functionality.
+test('', async ({ page }) => {
+
+});
+
+// Verify the messages for invalid login.
+test('', async ({ page }) => {
+
+});
+
+// Verify the ‘Remember Me’ functionality.
+test('', async ({ page }) => {
+
+});
+
+// Verify if the data in password field is either visible as asterisk or bullet signs.
+test('', async ({ page }) => {
+
+});
+
+// Verify if a user is able to login with a new password only after he/she has changed the password.
+test('', async ({ page }) => {
+
+});
+
+// Verify if the login page allows to log in simultaneously with different credentials in a different browser.
+test('', async ({ page }) => {
+
+});
+
+// Verify if the ‘Enter’ key of the keyboard is working correctly on the login page.
+test('', async ({ page }) => {
+
+});
